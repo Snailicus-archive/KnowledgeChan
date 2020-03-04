0d062107353c64ac607b9eb21eabda0508a40e89
Cutipus <Cutipus@protonmail.com>
2020-03-05 01:04:02 +0200

    a totally not total remake

--- bot.py
+++ bot.py
@@ -1,24 +1,159 @@
 import os
-
-import logic
+import re
+from pprint import pformat
 
 import discord
 from discord.ext import commands
 
+import logic
+
+# TODO: allow listing combinations by essence
+
+ADMIN_ROLES = 'Magic Society Official', 'Admin', 'Author'
+HELPSTRING = """**```Standard command list```**
+Use `?command` to use a command.
+
+__**Accessible by all**__:
+**0.** **`?help`**
+	prints this.
+
+**1.** **`?combine essence1 essence2 essence3`**
+	prints the confluence
+
+**2.** **`?list combinations`**
+	upload combinations database(csv file)
+
+**3.** **`?list combinations confluence_name`**
+	prints all the combinations leading to confluence
+
+**4.** **`?list essences`**
+	uploads essences database(csv file)
+
+__**Accessible only by  *Author* ,  *Owner*  and  *Magic Society Official*  roles**__:
+**5.** **`?new essence name [rarity]`**
+	add new essence to database. rarity is optional
+
+**6.** **`?new combination essence1 essence2 essence3 confluence`**
+	adds a new combination to the database
+	creates new confluence if it doesnt exist
+
+When there's trouble you know who to call.(lynx...)"""
+
+# this is a global. an intentional one.
 bot = commands.Bot(command_prefix='?')
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
     print('o am ready, master')
 
 @bot.command()
+async def help(ctx):
+    await ctx.send(HELPSTRING)
+
+@bot.command()
 async def mewl(ctx):
     await ctx.send('meow!')
 
-@bot.command()
-async def combine(ctx, first, second, third):
-    await ctx.send(logic.find_confluence(first, second, third))
 
+@bot.command()
+async def combine(ctx, a, b, c):
+    con, comb = logic.find_confluence(a, b, c)
+    print(logic.find_confluence(a, b, c))
+    await ctx.send(f'`{logic.show_combination(comb)} -> {con.name}`')
+
+@combine.error
+async def combine_error(ctx, error):
+    message = 'Wot?'
+    if isinstance(error, commands.MissingRequiredArgument):
+        message = 'List 3 essences.'
+    elif isinstance(error, commands.CommandInvokeError):
+        err = error.original
+        if isinstance(err, NameError):
+            message = f"Essence {str(err)} doesn't exist."
+        if isinstance(err, IndexError):
+            message = 'list 3 essences.'
+    await ctx.send(f'**ERROR**: `{message}`')
+    print(error)
+
+@bot.command()
+async def list(ctx, what, name=''):
+    what = what.lower()
+    name = logic.form(name)
+    if what in ('conf', 'confluences', 'combination', 'combinations'):
+        # list confluences
+        await list_confluences(ctx, name)
+    elif what in ('essences',):
+        await list_essences(ctx)
+    else:
+        raise commands.MissingRequiredArgument
+
+async def list_confluences(ctx, name=None):
+    if name:
+        await ctx.send(logic.confluences[name])
+    else:
+        with open(logic.COMBINATIONS_FILE, 'rb') as f:
+            await ctx.send('Included in file', file=discord.File(f, filename='combinations.csv'))
+
+async def list_essences(ctx):
+    with open(logic.ESSENCES_FILE, 'rb') as f:
+        await ctx.send('Included in file', file=discord.File(f, filename='essences.csv'))
+
+@list.error
+async def list_error(ctx, error):
+    message = 'Wot?'
+    if isinstance(error, commands.MissingRequiredArgument):
+        message = 'List confluences or essences.'
+    elif isinstance(error, commands.CommandInvokeError):
+        err = error.original
+        if isinstance(err, KeyError):
+            message = "Not found."
+    await ctx.send(f'**ERROR**: `{message}`')
+
+    print(error)
+
+# TODO: add "new" command line "list"
+@bot.command()
+@commands.check_any(commands.has_any_role(*ADMIN_ROLES), commands.is_owner())
+async def new(ctx, what, *args):
+    what = what.lower()
+    if what == 'essence':
+        await new_essence(ctx, *args)
+    elif what == 'combination':
+        await new_combination(ctx, *args)
+    else:
+        raise commands.MissingRequiredArgument
+
+async def new_essence(ctx, name, rarity='Unknown'):
+    name, rarity = map(logic.form, [name, rarity])
+    logic.add_essence(name, rarity)
+    await ctx.send(f'Added new essence `{name} [{rarity}]`.')
+
+
+# TODO: allow syntax as   a + b + c -> thing
+async def new_combination(ctx, a, b, c, name):
+    a, b, c = map(logic.form, [a, b, c])
+    logic.add_combination(a, b, c, name)
+    await ctx.send(f'Added combination `{a} + {b} + {c} -> {name.lower().capitalize()}`')
+
+@new.error
+async def new_error(ctx, error):
+    message = "I don't understand Q_Q"
+    if isinstance(error, commands.CheckFailure):
+        message = 'You are unworthy... Authorized personnel only.'
+    if isinstance(error, commands.MissingRequiredArgument):
+        message = 'Please add either new essence or new combination.'
+    elif isinstance(error, commands.CommandInvokeError):
+        err = error.original
+        if isinstance(err, NameError):
+            message = f'Essence {str(err)} is unrecognized.'
+        elif isinstance(err, IndexError):
+            message = 'Essences must be unique in combination'
+        elif isinstance(err, KeyError):
+            message = 'Its already a thing...'
+
+    await ctx.send(f'**ERROR**: `{message}`')
+    print(error)
 
 
 if __name__ == '__main__':
--- logic.py
+++ logic.py
@@ -1,59 +1,135 @@
 import csv
-
+from dataclasses import dataclass, field
 '''
 TODO:
-    write the actual bot
-    fix error in add combination
     print essences and combinations prettily - make it good
     search functionality for essences and confluences
 '''
 
+RARITIES = ['Unknown', 'Common', 'Uncommon', 'Rare', 'Epic', 'Legendary']
+ESSENCES_FILE = 'essences.csv'
+COMBINATIONS_FILE = 'combinations.csv'
 
-essences = {}
-combinations = {}
+
+@dataclass(order=True)
+class Essence:
+    sort_index: int = field(init=False, repr=False)
+    name: str
+    rarity: str = 'Unknown'
+    description: str = 'No info available(yet)'
+
+    def __hash__(self):
+        return
+    def __post_init__(self):
+        self.sort_index = RARITIES.index(self.rarity)
+
+    def __str__(self):
+        return f'{self.name} [{self.rarity}]'
+
+@dataclass
+class Confluence:
+    name: str
+    combinations: list
+    description: str = 'No info available(quite yet)'
+
+    def __str__(self):
+        return '\n'.join(f'{show_combination(comb)} -> {self.name}' for comb in  self.combinations)
+
+essences = {} # essence.name -> essence
+confluences = {} # confluence.name -> confluence
+combinations = {} # combination -> confluence
 
 def form(x):
-    return x.lower().capitalize()
+    return x.lower().capitalize().strip()
+
+def show_combination(combination):
+    combination = list(combination)
+    return ' + '.join(combination)
 
 def commit_essences():
-    with open('essences.csv', 'w', newline='') as essence_file:
+    with open(ESSENCES_FILE, 'w', newline='') as essence_file:
         writer = csv.writer(essence_file)
         for name, rarity in essences.items():
             writer.writerow([name, rarity])
 
 def commit_combinations():
-    with open('combinations.csv', 'w', newline='') as combinations_file:
+    with open(COMBINATIONS_FILE, 'w', newline='') as combinations_file:
         writer = csv.writer(combinations_file)
         for essences, confluence in combinations.items():
             writer.writerow(list(essences) + [confluence])
 
 def pull_essences():
-    with open('essences.csv', 'r', newline='') as essence_file:
+    with open(ESSENCES_FILE, 'r', newline='') as essence_file:
         reader = csv.reader(essence_file)
         for line in reader:
-            essences[line[0]] = line[1]
+            essence = Essence(line[0], line[1])
+            essences[essence.name] = essence
 
 def pull_combinations():
-    with open('combinations.csv', 'r', newline='') as combinations_file:
+    with open(COMBINATIONS_FILE, 'r', newline='') as combinations_file:
         reader = csv.reader(combinations_file)
         for line in reader:
-            combinations[frozenset(line[0:3])] = line[3]
+            name = line[3]
+            combination = frozenset(line[0:3])
+            confluence = confluences.get(name)
+            if not confluence:
+                confluence = Confluence(name, [combination])
+                confluences[name] = confluence
+            else:
+                confluence.combinations.append(combination)
+            combinations[combination] = confluence
+
+def create_combination(lst):
+    lst = list(map(form, lst))
+    for essence in lst:
+        if essence not in essences.keys():
+            raise NameError(essence)
+
+    comb = frozenset(lst)
+    if len(comb) != 3:
+        raise IndexError
+    return comb
 
 def find_confluence(a, b, c):
-    a, b, c = map(form, [a, b, c])
-    return combinations[frozenset([a, b, c])]
+    comb = create_combination([a, b, c])
+    return combinations[comb], comb
 
-def add_essence(name, rarity):
-    essences[form(name)] = form(rarity)
+def get_confluence(name):
+    return confluences.get(form(name))
 
-change_essence_rarity = add_essence
+def add_essence(name, rarity='Unknown'):
+    name, rarity = map(form, [name, rarity])
+    essence = Essence(name, rarity)
+
+    if name in essences.keys():
+        raise KeyError(name)
+
+    essences[name] = essence
+
+    with open(ESSENCES_FILE, 'a', newline='') as essences_file:
+        writer = csv.writer(essences_file)
+        writer.writerow([name, rarity])
 
 def add_combination(a, b, c, confluence):
-    a, b, c = map(form, [a, b, c])
-    if not all(essence in essences.keys() for essence in [a, b, c]):
-        raise Exception("missing essence")
-    combinations[frozenset([a, b, c])] = form(confluence)
+    name = form(confluence)
+    combination = create_combination([a, b, c])
 
+    # error if combination already assigned
+    if combination in combinations:
+        raise KeyError('combination exists')
+
+    # make new confluence if doesnt exist
+    conf = confluences.get(name)
+    if not conf:
+        conf = Confluence(name, [])
+        confluences[name] = conf
+
+    combinations[combination] = conf
+    conf.combinations.append(combination)
+
+    with open(COMBINATIONS_FILE, 'a', newline='') as comb_file:
+        writer = csv.writer(comb_file)
+        writer.writerow([a, b, c, name])
 
 if __name__ == '__main__':
     pull_essences()
